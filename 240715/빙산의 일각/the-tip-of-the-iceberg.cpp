44b6eb3e169af86f3b4484a3c326c57066c83a1c
CERabi <hgs6417@gmail.com>
2024-07-15 17:52:28 +0900

    Title: 빙산의 일각, Time: 1002ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240715/빙산의 일각/the-tip-of-the-iceberg.cpp
+++ 240715/빙산의 일각/the-tip-of-the-iceberg.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main() {
-    int N, H[100005] = { 0 }, ptr[100005] = { 0 }, max = -1, when;
+    int N, H[100005] = { 0 }, ptr[100005] = { 0 }, max = -1;
     cin >> N;
     for(int i = 0; i < N; i++) {
         cin >> H[i]; ptr[i] = H[i];
     }
     sort(ptr, ptr + N);
     for(int i = 0; i < N; i++) {
-        int prev = 1, cnt = 0; // prev 0이면 수면 아래, 1이면 수면 위
+        int prev = 0, cnt = 0; // prev 0이면 수면 아래, 1이면 수면 위
         for(int j = 0; j < N; j++) {
             if(ptr[i] >= H[j]) { // 수면 아래면
                 if(prev == 1) cnt++; // 수면 위였을 때 빙산 추가
